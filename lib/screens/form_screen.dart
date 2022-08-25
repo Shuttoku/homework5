c2ad6a4c24e94976b9fb9a43ca1ca81b0827c554
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-08-25 23:10:35 +0700

    oneelocojxxj

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -3,19 +3,40 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("แบบฟอร์มบันทึกข้อมูล"),
         ),
-        body: Form(
-          child: Column(children: [
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+            key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            
+            children: [
             TextFormField(
               decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+              autofocus: true,
+              validator: (String? str){
+                if(str!.isEmpty){
+                  return "กรุณาป้อนชื่อรายการ";
+                }
+                return null;
+              },
             ),
             TextFormField(
               decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+              keyboardType:TextInputType.number,
+              validator: (String? str){
+                if(str!.isEmpty){
+                  return "กรุณาป้อนจำนวนเงิน";
+                }
+                return null;
+              },
             ), ////
             FlatButton
             (
@@ -23,10 +44,14 @@ class FormScreen extends StatelessWidget {
               color: Colors.pink,
               textColor: Colors.white,
               onPressed: (){
+                if(formKey.currentState!.validate()){
+
+                }
                   Navigator.pop(context);
               }
             )
           ]),
-        ));
+        )
+    ));
   }
 }
